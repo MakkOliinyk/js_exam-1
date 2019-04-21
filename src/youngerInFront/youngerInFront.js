7318e9d0df0d3e28c4430c8e0751393c9f0089f9
Roman Apostol <roman@mate.academy>
2019-04-21 19:11:25 +0300

    Update src/youngerInFront/youngerInFront.js
    Co-Authored-By: ivadimko <vvadimko@gmail.com>

--- src/youngerInFront/youngerInFront.js
+++ src/youngerInFront/youngerInFront.js
@@ -4,7 +4,7 @@
  * Implement function youngerInFront:
  *
  * Function takes a natural number N and returns an array with its digits in
- * order from youngest to oldest. Youngest means the last digit in number and
+ * order from least significant to most significant. Least significant means the last digit in a number and most significant means the first one.
  * oldest means the first one.
  *
  * e.g. for N = 46156 it should return [6, 5, 1, 6, 4]. Make sure all items
