951d7790e234d906f969298b9976e46f8dc812ce
Roman Apostol <roman@mate.academy>
2019-04-21 19:09:26 +0300

    Update src/youngerInFront/youngerInFront.test.js
    Co-Authored-By: ivadimko <vvadimko@gmail.com>

--- src/youngerInFront/youngerInFront.test.js
+++ src/youngerInFront/youngerInFront.test.js
@@ -8,7 +8,7 @@ describe('Testing youngerInFront', () => {
       .toEqual([1]);
   });
 
-  test('Should return [1, 2] for: 21', () => {
+  test('Should return the array of digits correctly for two-digit numbers.', () => {
     expect(youngerInFront(21))
       .toEqual([1, 2]);
   });
